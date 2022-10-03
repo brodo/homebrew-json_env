394cb776aa683024b2aeed04b4919a9d2d982d87
Julian Dax <julian.dax@posteo.de>
2022-10-03 22:46:54 +0200

    updating formula for json_env

--- Formula/json_env.rb
+++ Formula/json_env.rb
@@ -4,9 +4,9 @@
 class JsonEnv < Formula
     desc "json_env is dotenv, but with JSON." 
     homepage "https://github.com/brodo/json_env"
-    url "https://github.com/brodo/json_env/archive/refs/tags/v1.1.12.zip"
-    sha256 "fd216fa90c98d98d4b228978de46c3a35e33ad6bd914c5420ddbe74ed071253e"
-    version "1.1.12"
+    url "https://github.com/brodo/json_env/archive/refs/tags/v1.1.13.zip"
+    sha256 "3319787c02287bce0fe66cb2afe5f2e8073684072705e6099728f024af2c8e3c"
+    version "1.1.13"
     depends_on "rust" => :build
   
     def install
